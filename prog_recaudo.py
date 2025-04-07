34105e3f4b34d0022c9bf3104fb265dca626d22d
Alejo141 <66750054+Alejo141@users.noreply.github.com>
2025-04-07 10:11:11 -0500

    Add files via upload

--- prog_recaudo.py
+++ prog_recaudo.py
@@ -113,6 +113,8 @@ if opcion == "Recaudo":
 
                 #solo_df1 = sum_val_movil[~sum_val_movil["CC"].isin(sum_siigo['IDENTIFICACION'])]
                 #st.dataframe(solo_df1)
+                st.write("Columnas en sum_val_movil:", sum_val_movil.columns.tolist())
+                st.write("Columnas en sum_siigo:", sum_siigo.columns.tolist())
                 solo_df1 = sum_val_movil.merge(sum_siigo, on="CC", how="left", suffixes=("_sum_val_movil", "_sum_siigo"))
                 st.dataframe(solo_df1)
                 no_en_sum_siigo = solo_df1[solo_df1["DÉBITO"].isna()]
