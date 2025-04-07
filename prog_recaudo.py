537f8013e7cbf16505b944f64989c031ee89b1c3
Alejo141 <66750054+Alejo141@users.noreply.github.com>
2025-04-07 09:03:35 -0500

    Add files via upload

--- prog_recaudo.py
+++ prog_recaudo.py
@@ -18,7 +18,8 @@ def generar_xlsx(df1, df2):
     output = BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         df1.to_excel(writer, sheet_name='Datos_Cruzados', index=False)
-        df2.to_excel(writer, sheet_name='Resumen_Recaudo', startrow= 2, startcol=3, index=False)
+        df2.to_excel(writer, sheet_name='Resumen_Recaudo', startrow= 1, startcol=1, index=False)
+        df2.to_excel(writer, sheet_name='Resumen_Recaudo', startrow= 1, startcol=5, index=False)
     output.seek(0)
     return output
 
